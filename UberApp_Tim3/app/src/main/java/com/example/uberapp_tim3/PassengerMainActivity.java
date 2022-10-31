8770882c8314d41e8d2d36dd97c3e691d55ad529
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-10-31 11:33:09 +0100

    Implemented buttons for Account and Inbox Activities

--- UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/PassengerMainActivity.java
+++ UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/PassengerMainActivity.java
@@ -2,7 +2,10 @@ package com.example.uberapp_tim3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PassengerMainActivity extends AppCompatActivity {
 
@@ -10,6 +13,25 @@ public class PassengerMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger_main);
+
+        Button btnAccount = findViewById(R.id.btnAccount);
+        Button btnInbox = findViewById(R.id.btnInbox);
+
+        btnAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PassengerMainActivity.this, PassengerAccountActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnInbox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PassengerMainActivity.this, PassengerInboxActivity.class);
+                startActivity(intent);
+            }
+        });
     }
     @Override
     protected void onStart() {
