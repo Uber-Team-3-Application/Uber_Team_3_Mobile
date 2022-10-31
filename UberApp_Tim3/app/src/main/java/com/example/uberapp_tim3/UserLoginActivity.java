e30d2770d45e0d2d3d52fd6b1c2974ff9ecbee62
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-10-31 11:06:40 +0100

    Created new Activities

---
+++ UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/UserLoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.uberapp_tim3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_login);
+    }
+}
