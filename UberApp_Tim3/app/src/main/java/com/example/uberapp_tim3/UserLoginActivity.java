3ad259c9737c49ebb183944496d113cf50d55eb1
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-10-31 11:24:16 +0100

    Implemented switching activities in login Activity

--- UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/UserLoginActivity.java
+++ UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/UserLoginActivity.java
@@ -2,7 +2,10 @@ package com.example.uberapp_tim3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserLoginActivity extends AppCompatActivity {
 
@@ -10,6 +13,25 @@ public class UserLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+
+        Button btnRegister = findViewById(R.id.btnRegister);
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserLoginActivity.this, PassengerRegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserLoginActivity.this, PassengerMainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
     @Override
     protected void onStart() {
