422809a7db4b7cea1296553ee23bfe74f0418963
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-11-01 08:43:31 +0100

    Added ToggleButton in Driver Main

--- UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/DriverMainActivity.java
+++ UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/DriverMainActivity.java
@@ -3,6 +3,9 @@ package com.example.uberapp_tim3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class DriverMainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class DriverMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_main);
+
+
+        ToggleButton tbChangeActivity = findViewById(R.id.btnToggleOnline);
+
+        tbChangeActivity.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                String text = "Current status: ";
+                if(b) text += "Online";
+                else text += "Offline";
+                Toast.makeText(DriverMainActivity.this, text, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
