c3f30387626c5c79aab7870e23fe7db5d03967ea
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-11-01 08:28:58 +0100

    Created Driver Main Activity

--- UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/PassengerRegisterActivity.java
+++ UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/PassengerRegisterActivity.java
@@ -10,6 +10,7 @@ public class PassengerRegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger_register);
+        
     }
     @Override
     protected void onStart() {
