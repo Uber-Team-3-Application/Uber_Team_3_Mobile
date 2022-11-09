eb0a044b2529af33ae463487feb602f1b7a99819
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2022-11-09 22:25:02 +0100

    added toolbar and icons to DriverMainActivity

--- UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/DriverMainActivity.java
+++ UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/DriverMainActivity.java
@@ -1,19 +1,42 @@
 package com.example.uberapp_tim3;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import com.example.uberapp_tim3.fragments.DriverFragment;
+import com.example.uberapp_tim3.model.NavItem;
+import com.example.uberapp_tim3.tools.FragmentTransition;
+
+import java.util.ArrayList;
+
 public class DriverMainActivity extends AppCompatActivity {
+
+    private ArrayList<NavItem> mNavItems = new ArrayList<NavItem>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_main);
+        prepareMenu(mNavItems);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar actionBar = getSupportActionBar();
+
+        /* Postavljamo ikonicu unutar toolbar-a*/
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_drawer);
+            actionBar.setHomeButtonEnabled(true);
+        }
 
         ToggleButton tbChangeActivity = findViewById(R.id.btnToggleOnline);
 
@@ -26,5 +49,39 @@ public class DriverMainActivity extends AppCompatActivity {
                 Toast.makeText(DriverMainActivity.this, text, Toast.LENGTH_SHORT).show();
             }
         });
+
+        if (savedInstanceState == null) {
+            selectItemFromDrawer(0);
+        }
+    }
+
+    private void selectItemFromDrawer(int position) {
+        if(position == 0){
+            FragmentTransition.to(DriverFragment.newInstance(), this, false);
+        }else if(position == 1){
+          //  FragmentTransition.to(ExamplesFragment.newInstance(), this, false);
+
+        }else if(position == 2){
+            //..
+        }else if(position == 3){
+            //..
+        }else if(position == 4){
+            //..
+        }else if(position == 5){
+            //...
+        }else{
+            Log.e("DRAWER", "Nesto van opsega!");
+        }
+
+
+    }
+
+
+    private void prepareMenu(ArrayList<NavItem> mNavItems ) {
+//        mNavItems.add(new NavItem(getString(R.string.home), getString(R.string.home_long), R.drawable.ic_action_map));
+//        mNavItems.add(new NavItem(getString(R.string.places), getString(R.string.places_long), R.drawable.ic_action_place));
+//        mNavItems.add(new NavItem(getString(R.string.preferences), getString(R.string.preferences_long), R.drawable.ic_action_settings));
+//        mNavItems.add(new NavItem(getString(R.string.about), getString(R.string.about_long), R.drawable.ic_action_about));
+//        mNavItems.add(new NavItem(getString(R.string.sync_data), getString(R.string.sync_data_long), R.drawable.ic_action_refresh));
     }
 }
