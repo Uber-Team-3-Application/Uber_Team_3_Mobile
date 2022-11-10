dcc7fb65f53c90ec78ed1e31144030dd6422b94a
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-11-10 16:24:40 +0100

    Edited Splash Activity

--- UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/SplashActivity.java
+++ UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/SplashActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.widget.ProgressBar;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -14,8 +16,9 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
         int SPLASH_TIME_OUT = 5000;
+
+
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
@@ -23,6 +26,8 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         }, SPLASH_TIME_OUT);
+
+
     }
     @Override
     protected void onStart() {
