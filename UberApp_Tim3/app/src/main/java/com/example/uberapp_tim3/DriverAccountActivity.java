fd22c59942343f75ba46e8b64f43035ae3b426b6
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-11-10 16:37:02 +0100

    Created Driver Account Activity

---
+++ UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/DriverAccountActivity.java
@@ -0,0 +1,16 @@
+package com.example.uberapp_tim3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DriverAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_account);
+    }
+
+
+}
