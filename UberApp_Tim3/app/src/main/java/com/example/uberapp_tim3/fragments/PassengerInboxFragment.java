f2e91251aded78d99f5c7407254c2d55fd1fa951
Jelena Mikovic <jelena.mikovic189@gmail.com>
2022-11-12 18:27:22 +0100

    Small changes

--- UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/fragments/PassengerInboxFragment.java
+++ UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/fragments/PassengerInboxFragment.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.uberapp_tim3.R;
 
@@ -16,7 +17,14 @@ public class PassengerInboxFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.activity_passenger_inbox, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final ListView listView = (ListView) getView().findViewById(R.id.inbox_listview);
+        //listView.setAdapter();
+    }
 }
