e4f79de9883b79b25f0e2978e59bca64acb8d266
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-11-13 12:55:46 +0100

    Created Fragments for editing info

---
+++ UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/fragments/DriverEditInfoFragment.java
@@ -0,0 +1,32 @@
+package com.example.uberapp_tim3.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.uberapp_tim3.R;
+
+
+public class DriverEditInfoFragment extends Fragment {
+
+
+    public DriverEditInfoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_driver_edit_info, container, false);
+    }
+}
