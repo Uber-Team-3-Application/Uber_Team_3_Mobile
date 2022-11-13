d3a8c1e8bed26dbb4ff477fe19dfff1d73acd574
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-11-13 17:23:51 +0100

    Created Favourite Routes Fragment

---
+++ UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/fragments/PassengerFavouriteRoutesFragment.java
@@ -0,0 +1,41 @@
+package com.example.uberapp_tim3.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.uberapp_tim3.R;
+
+
+public class PassengerFavouriteRoutesFragment extends Fragment {
+
+
+    public PassengerFavouriteRoutesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_passenger_favourite_routes, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
