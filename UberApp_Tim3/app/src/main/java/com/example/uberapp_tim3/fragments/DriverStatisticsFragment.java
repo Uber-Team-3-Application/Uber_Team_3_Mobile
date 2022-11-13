6e6455951bfcde534046f3621fc28d904624a24d
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-11-13 16:25:07 +0100

    Implemented logic for Driver Statistics

--- UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/fragments/DriverStatisticsFragment.java
+++ UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/fragments/DriverStatisticsFragment.java
@@ -4,14 +4,20 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.util.Pair;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import com.example.uberapp_tim3.R;
+import com.example.uberapp_tim3.tools.FragmentTransition;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 
 
 public class DriverStatisticsFragment extends Fragment {
@@ -36,8 +42,25 @@ public class DriverStatisticsFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        ((RadioButton)getActivity().findViewById(R.id.rbDaily)).setChecked(true);
         setOnClickListeners();
         super.onViewCreated(view, savedInstanceState);
+        Button calendarButton = getView().findViewById(R.id.btnDateRange);
+        TextView selectedDate = getView().findViewById(R.id.tvSelectedDate);
+        MaterialDatePicker materialDatePicker = MaterialDatePicker.Builder.dateRangePicker().setTitleText("Select").setSelection(Pair.create(MaterialDatePicker.thisMonthInUtcMilliseconds(), MaterialDatePicker.todayInUtcMilliseconds())).build();
+        calendarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                materialDatePicker.show(getActivity().getSupportFragmentManager(), "Tag_picker");
+                materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+                    @Override
+                    public void onPositiveButtonClick(Object selection) {
+                        selectedDate.setText(materialDatePicker.getHeaderText());
+                    }
+                });
+            }
+        });
     }
 
     private void setOnClickListeners(){
