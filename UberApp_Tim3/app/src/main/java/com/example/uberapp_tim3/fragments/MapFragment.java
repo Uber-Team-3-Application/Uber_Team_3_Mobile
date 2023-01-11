e0dd91007925af25e1dbb3b9dfa468622a662e59
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-01-11 14:46:17 +0100

    Created map fragment

---
+++ UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/fragments/MapFragment.java
@@ -0,0 +1,65 @@
+package com.example.uberapp_tim3.fragments;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.uberapp_tim3.R;
+
+import java.util.List;
+
+
+public class MapFragment extends Fragment implements LocationListener {
+
+
+
+    public MapFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull List<Location> locations) {
+        LocationListener.super.onLocationChanged(locations);
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        LocationListener.super.onProviderEnabled(provider);
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        LocationListener.super.onProviderDisabled(provider);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
+    }
+}
