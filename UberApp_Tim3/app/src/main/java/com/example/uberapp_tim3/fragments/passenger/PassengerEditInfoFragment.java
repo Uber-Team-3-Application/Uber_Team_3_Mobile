79b34489bfd1649117e505b9e2a940a09965bbbb
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-01-11 20:25:47 +0100

    Added edditing profile info

--- UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/fragments/passenger/PassengerEditInfoFragment.java
+++ UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/fragments/passenger/PassengerEditInfoFragment.java
@@ -22,12 +22,15 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.uberapp_tim3.R;
 import com.example.uberapp_tim3.model.DTO.PassengerDTO;
 import com.example.uberapp_tim3.model.mockup.Passenger;
 import com.example.uberapp_tim3.services.ServiceUtils;
 import com.example.uberapp_tim3.tools.PassengerMockup;
+
+import org.w3c.dom.Text;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import retrofit2.Call;
@@ -40,6 +43,12 @@ public class PassengerEditInfoFragment extends Fragment {
     private Button btnChangeAvatar;
     private SharedPreferences sharedPreferences;
     private CircleImageView imgAvatar;
+    private TextView tvName;
+    private TextView tvSurname;
+    private TextView tvEmail;
+    private TextView tvAddress;
+    private TextView tvNumber;
+    private String avatarBase64;
     public PassengerEditInfoFragment() {
         // Required empty public constructor
     }
@@ -50,11 +59,6 @@ public class PassengerEditInfoFragment extends Fragment {
         super.onCreate(savedInstanceState);
         imgAvatar = getActivity().findViewById(R.id.imgEditPassengerAvatar);
         btnChangeAvatar = getActivity().findViewById(R.id.btnChangePassengerAvatar);
-        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            btnChangeAvatar.setEnabled(false);
-            ActivityCompat.requestPermissions(getActivity(), new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
-        }
-
     }
 
     @Override
@@ -73,29 +77,30 @@ public class PassengerEditInfoFragment extends Fragment {
                 if(!response.isSuccessful()) return;
                 PassengerDTO passenger = response.body();
 
-                TextView tvName = getActivity().findViewById(R.id.txtPassengerEditFirstName);
+                tvName = getActivity().findViewById(R.id.txtPassengerEditFirstName);
                 tvName.setText(passenger.getName());
 
-                TextView tvSurname = getActivity().findViewById(R.id.txtPassengerEditLastName);
+                tvSurname = getActivity().findViewById(R.id.txtPassengerEditLastName);
                 tvSurname.setText(passenger.getSurname());
 
-                TextView tvPhoneNumber = getActivity().findViewById(R.id.txtPassengerEditPhoneNumber);
-                tvPhoneNumber.setText(passenger.getTelephoneNumber());
+                tvNumber = getActivity().findViewById(R.id.txtPassengerEditPhoneNumber);
+                tvNumber.setText(passenger.getTelephoneNumber());
 
-                TextView emailAddress = getActivity().findViewById(R.id.txtPassengerEmailEdit);
-                emailAddress.setText(passenger.getEmail());
+                tvEmail = getActivity().findViewById(R.id.txtPassengerEmailEdit);
+                tvEmail.setText(passenger.getEmail());
 
-                TextView homeAddress = getActivity().findViewById(R.id.txtHomeAddressEdit);
-                homeAddress.setText(passenger.getAddress());
+                tvAddress = getActivity().findViewById(R.id.txtHomeAddressEdit);
+                tvAddress.setText(passenger.getAddress());
 
-                if(!passenger.getProfilePicture().contains(",")){return;}
+                if(!passenger.getProfilePicture().contains(",")){ avatarBase64 = "profilna";return;}
 
+                avatarBase64 = passenger.getProfilePicture();
                 String base64Image = passenger.getProfilePicture().split(",")[1];
                 byte[] decodedString = Base64.decode(base64Image, Base64.DEFAULT);
                 Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
 
-                CircleImageView cv = getActivity().findViewById(R.id.imgEditPassengerAvatar);
-                cv.setImageBitmap(decodedByte);
+                imgAvatar = getActivity().findViewById(R.id.imgEditPassengerAvatar);
+                imgAvatar.setImageBitmap(decodedByte);
             }
 
             @Override
@@ -136,7 +141,42 @@ public class PassengerEditInfoFragment extends Fragment {
                 getActivity().getSupportFragmentManager().popBackStackImmediate();
             }
         });
-        ((Button) getActivity().findViewById(R.id.btnAcceptEditPassenger)).setOnClickListener(null);
+        ((Button) getActivity().findViewById(R.id.btnAcceptEditPassenger)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PassengerDTO passenger = new PassengerDTO();
+                passenger.setConfirmedMail(true);
+                passenger.setAddress(tvAddress.getText().toString());
+                passenger.setTelephoneNumber(tvNumber.getText().toString());
+                passenger.setEmail(tvEmail.getText().toString());
+                passenger.setName(tvName.getText().toString());
+                passenger.setSurname(tvSurname.getText().toString());
+                passenger.setId(sharedPreferences.getLong("pref_id", 102));
+                passenger.setProfilePicture(avatarBase64);
+                Call<PassengerDTO> call = ServiceUtils.passengerService.updatePassenger(passenger.getId(), passenger);
+                call.enqueue(new Callback<PassengerDTO>() {
+                    @Override
+                    public void onResponse(Call<PassengerDTO> call, Response<PassengerDTO> response) {
+                        if(!response.isSuccessful()){
+                            Toast.makeText(null, "Something went wrong!", Toast.LENGTH_SHORT).show();
+                        }
+                        TextView tvName = getActivity().findViewById(R.id.passengerNameNavigation);
+                        String fullName = passenger.getName() + " " + passenger.getSurname();
+                        tvName.setText(fullName);
+
+                        TextView tvPhoneNumber = getActivity().findViewById(R.id.passengerPhoneNavigation);
+                        tvPhoneNumber.setText(passenger.getTelephoneNumber());
+                        getActivity().getSupportFragmentManager().popBackStackImmediate();
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<PassengerDTO> call, Throwable t) {
+                        Toast.makeText(null, t.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
 
     }
 
--- UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/services/interfaces/IPassengerService.java
+++ UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/services/interfaces/IPassengerService.java
@@ -5,8 +5,10 @@ import com.example.uberapp_tim3.model.DTO.UserDTO;
 import com.example.uberapp_tim3.services.ServiceUtils;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface IPassengerService {
@@ -19,4 +21,7 @@ public interface IPassengerService {
     )
     @GET(ServiceUtils.passenger + "/{id}")
     Call<PassengerDTO> getPassenger(@Path("id") Long id);
+
+    @PUT(ServiceUtils.passenger + "/{id}")
+    Call<PassengerDTO> updatePassenger(@Path("id") Long id, @Body PassengerDTO passenger);
 }
