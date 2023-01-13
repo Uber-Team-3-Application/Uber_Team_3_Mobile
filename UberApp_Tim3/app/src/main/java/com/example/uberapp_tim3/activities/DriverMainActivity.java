63c1cf673148872dd85b2295778de0a40d7f9bcc
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2023-01-13 11:03:18 +0100

    added auto changed activity

--- UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/activities/DriverMainActivity.java
+++ UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/activities/DriverMainActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.AlarmManager;
 import android.app.Dialog;
@@ -16,10 +18,12 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -30,14 +34,17 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.uberapp_tim3.R;
 import com.example.uberapp_tim3.adapters.DrawerNavListAdapter;
 import com.example.uberapp_tim3.fragments.AccountSettingsFragment;
+import com.example.uberapp_tim3.fragments.MapFragment;
 import com.example.uberapp_tim3.fragments.driver.DriverAccountFragment;
 import com.example.uberapp_tim3.fragments.driver.DriverHomeFragment;
 import com.example.uberapp_tim3.fragments.driver.DriverInboxFragment;
 import com.example.uberapp_tim3.fragments.driver.DriverRideHistoryFragment;
+import com.example.uberapp_tim3.model.DTO.DriverActivityDTO;
 import com.example.uberapp_tim3.model.DTO.UserDTO;
 import com.example.uberapp_tim3.services.ServiceUtils;
 import com.example.uberapp_tim3.tools.NavItem;
@@ -55,6 +62,8 @@ import retrofit2.Response;
 public class DriverMainActivity extends AppCompatActivity {
 
     private final static String DRIVER_CHANEL = "Driver channel";
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
 
 
     private ArrayList<NavItem> mNavItems = new ArrayList<NavItem>();
@@ -63,7 +72,6 @@ public class DriverMainActivity extends AppCompatActivity {
     private ListView mDrawerList;
     private ActionBarDrawerToggle mDrawerToggle;
     private CharSequence mTitle;
-    public static String SYNC_DATA = "SYNC_DATA";
 
     private String synctime;
     private boolean allowSync;
@@ -71,10 +79,12 @@ public class DriverMainActivity extends AppCompatActivity {
     private AlarmManager manager;
     private SharedPreferences sharedPreferences;
 
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.fragment_driver_main);
         prepareMenu(mNavItems);
 
@@ -132,13 +142,43 @@ public class DriverMainActivity extends AppCompatActivity {
 
     }
 
+    private void changeActivity(Long id, boolean isActive) {
+        Call<String> call = ServiceUtils.driverService.changeActivity(id, new DriverActivityDTO(isActive));
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                Toast.makeText(getApplicationContext(), "You are active!", Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                Log.d("Fail", t.getMessage());
+            }
+        });
+    }
+
+
     @Override
-    protected void onResume() {
-        super.onResume();
-        getDriver(sharedPreferences.getLong("pref_id", 0));
+    protected void onDestroy() {
+        super.onDestroy();
+        changeActivity(sharedPreferences.getLong("pref_id", 0L), false);
 
 
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getDriver(sharedPreferences.getLong("pref_id", 0L));
+        changeActivity(sharedPreferences.getLong("pref_id", 0L), true);
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        boolean gps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean wifi = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        Log.i("wwww", String.valueOf(gps));
+        Log.i("wqqqq", String.valueOf(wifi));
+    }
+
     public UserDTO getDriver(Long id){
 
         Call<UserDTO> call = ServiceUtils.driverService.getDriver(id);
@@ -183,7 +223,7 @@ public class DriverMainActivity extends AppCompatActivity {
     }
 
     private void consultPreferences() {
-        sharedPreferences = getSharedPreferences("com.example.uberapp_tim3_preferences", Context.MODE_PRIVATE);
+        sharedPreferences = getSharedPreferences("preferences", Context.MODE_PRIVATE);
         if(sharedPreferences.contains("pref_sync_list")){
             synctime = sharedPreferences.getString("pref_sync_list", "1");
             if(sharedPreferences.contains("pref_sync")){
@@ -193,7 +233,7 @@ public class DriverMainActivity extends AppCompatActivity {
     }
 
     private void setPreferences(){
-        sharedPreferences = getSharedPreferences("com.example.vezbe6_preferences", Context.MODE_PRIVATE);
+        sharedPreferences = getSharedPreferences("preferences", Context.MODE_PRIVATE);
         SharedPreferences.Editor sp_editor = sharedPreferences.edit();
         if(!sharedPreferences.contains("pref_name")){
             sp_editor.putString("pref_name", "Ressen");
@@ -210,13 +250,13 @@ public class DriverMainActivity extends AppCompatActivity {
 
     private void selectItemFromDrawer(int position) {
 
-        Handler handler = new Handler();
-        handler.postDelayed(() -> {
-            Dialog dialog = new Dialog(this);
-            dialog.setContentView(R.layout.popup_driver);
-            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-            dialog.show();
-        }, 50000);
+//        Handler handler = new Handler();
+//        handler.postDelayed(() -> {
+//            Dialog dialog = new Dialog(this);
+//            dialog.setContentView(R.layout.popup_driver);
+//            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//            dialog.show();
+//        }, 50000);
         if (position == 0) {
             FragmentTransition.to(DriverAccountFragment.newInstance(), this, true);
         } else if (position == 1) {
@@ -224,7 +264,8 @@ public class DriverMainActivity extends AppCompatActivity {
         } else if (position == 2) {
             FragmentTransition.to(DriverInboxFragment.newInstance(), this, true);
         } else if (position == 4) {
-            FragmentTransition.to(DriverHomeFragment.newInstance(), this, false);
+            FragmentTransition.to(MapFragment.newInstance(), this, false);
+//            FragmentTransition.to(DriverHomeFragment.newInstance(), this, false);
         } else if (position == 3) {
             FragmentTransition.to(AccountSettingsFragment.newInstance(), this, true);
         } else {
@@ -253,6 +294,40 @@ public class DriverMainActivity extends AppCompatActivity {
         mNavItems.add(new NavItem(getString(R.string.preferences), getString(R.string.preferences_long), R.drawable.ic_baseline_settings_24));
         mNavItems.add(new NavItem(getString(R.string.driver_back), getString(R.string.driver_back_long), R.drawable.ic_home));
         mNavItems.add(new NavItem(getString(R.string.log_out) , getString(R.string.logout), R.drawable.ic_logout));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "LOCATION GRANTED", Toast.LENGTH_SHORT).show();
+                    }
+
+                } else if (grantResults.length > 0
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "LOCATION GRANTED", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                }
+            }
+
+        }
     }
 
 
