217bef375d2fed694d88f2a4eb992865cf0e874f
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2023-01-13 11:03:34 +0100

    added draw route fragment

---
+++ UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/fragments/DrawRouteFragment.java
@@ -0,0 +1,150 @@
+package com.example.uberapp_tim3.fragments;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.uberapp_tim3.BuildConfig;
+import com.example.uberapp_tim3.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DirectionsLeg;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.DirectionsStep;
+import com.google.maps.model.EncodedPolyline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DrawRouteFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private String TAG = "so47492459";
+    private SupportMapFragment mMapFragment;
+
+
+    public static DrawRouteFragment newInstance() {
+
+        DrawRouteFragment mpf = new DrawRouteFragment();
+
+        return mpf;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapFragment = SupportMapFragment.newInstance();
+
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.replace(R.id.map_container, mMapFragment).commit();
+
+        //pozivamo ucitavnje mape.
+        //VODITI RACUNA OVO JE ASINHRONA OPERACIJA
+        //LOKACIJE MOGU DA SE DOBIJU PRE MAPE I OBRATNO
+        mMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup vg, Bundle data) {
+        setHasOptionsMenu(true);
+        View view = inflater.inflate(R.layout.map_layout, vg, false);
+
+        return view;
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng barcelona = new LatLng(41.385064,2.173403);
+        mMap.addMarker(new MarkerOptions().position(barcelona).title("Marker in Barcelona"));
+
+        LatLng madrid = new LatLng(40.416775,-3.70379);
+        mMap.addMarker(new MarkerOptions().position(madrid).title("Marker in Madrid"));
+
+        LatLng zaragoza = new LatLng(41.648823,-0.889085);
+
+        //Define list to get all latlng for the route
+        List<LatLng> path = new ArrayList();
+
+
+        //Execute Directions API request
+        GeoApiContext context = new GeoApiContext.Builder()
+                .apiKey(BuildConfig.MAPS_API_KEY)
+                .build();
+        DirectionsApiRequest req = DirectionsApi.getDirections(context, barcelona.latitude + "," + barcelona.longitude,
+                madrid.latitude + "," + madrid.longitude);
+
+        try {
+            DirectionsResult res = req.await();
+            //Loop through legs and steps to get encoded polylines of each step
+            if (res.routes != null && res.routes.length > 0) {
+                DirectionsRoute route = res.routes[0];
+
+                if (route.legs !=null) {
+                    for(int i=0; i<route.legs.length; i++) {
+                        DirectionsLeg leg = route.legs[i];
+                        if (leg.steps != null) {
+                            for (int j=0; j<leg.steps.length;j++){
+                                DirectionsStep step = leg.steps[j];
+                                if (step.steps != null && step.steps.length >0) {
+                                    for (int k=0; k<step.steps.length;k++){
+                                        DirectionsStep step1 = step.steps[k];
+                                        EncodedPolyline points1 = step1.polyline;
+                                        if (points1 != null) {
+                                            //Decode polyline and add points to list of route coordinates
+                                            List<com.google.maps.model.LatLng> coords1 = points1.decodePath();
+                                            for (com.google.maps.model.LatLng coord1 : coords1) {
+                                                path.add(new LatLng(coord1.lat, coord1.lng));
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    EncodedPolyline points = step.polyline;
+                                    if (points != null) {
+                                        //Decode polyline and add points to list of route coordinates
+                                        List<com.google.maps.model.LatLng> coords = points.decodePath();
+                                        for (com.google.maps.model.LatLng coord : coords) {
+                                            path.add(new LatLng(coord.lat, coord.lng));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch(Exception ex) {
+            Log.e(TAG, ex.getLocalizedMessage());
+        }
+
+        //Draw the polyline
+        if (path.size() > 0) {
+            PolylineOptions opts = new PolylineOptions().addAll(path).color(Color.BLUE).width(5);
+            mMap.addPolyline(opts);
+        }
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(zaragoza, 6));
+    }
+}
