6c9f93df21f8cea46c5d41503b6b3d972a14361f
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-01-13 09:27:58 +0100

    Added displaying real passenger data

--- UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/fragments/passenger/PassengerInfoProfile.java
+++ UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/fragments/passenger/PassengerInfoProfile.java
@@ -1,19 +1,31 @@
 package com.example.uberapp_tim3.fragments.passenger;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.uberapp_tim3.R;
+import com.example.uberapp_tim3.model.DTO.PassengerDTO;
 import com.example.uberapp_tim3.model.mockup.Passenger;
+import com.example.uberapp_tim3.services.ServiceUtils;
 import com.example.uberapp_tim3.tools.PassengerMockup;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class PassengerInfoProfile extends Fragment {
 
@@ -28,37 +40,55 @@ public class PassengerInfoProfile extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        setTextViews();
         Bundle bundle  = this.getArguments();
 
         assert bundle != null;
-        Long passengerID= bundle.getLong("passengerID");
-        setTextViews();
+        Long passengerID= bundle.getLong("passengerId");
+        Log.d("Passenger ID", Long.toString(passengerID));
+        Call<PassengerDTO> call = ServiceUtils.passengerService.getPassenger(passengerID);
+        call.enqueue(new Callback<PassengerDTO>() {
+            @Override
+            public void onResponse(Call<PassengerDTO> call, Response<PassengerDTO> response) {
+                if(!response.isSuccessful()){
+                    Toast.makeText(getActivity(), "Can't get passenger info.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                PassengerDTO passenger = response.body();
+                assert passenger != null;
+                setTextViews(passenger);
+
+            }
+
+            @Override
+            public void onFailure(Call<PassengerDTO> call, Throwable t) {
+                Log.d("FAIL", "Something went wrong!");
+            }
+        });
 
     }
 
-    private void setTextViews(){
-        Passenger passenger = PassengerMockup.getPassengers().get(0);
+    private void setTextViews(PassengerDTO passenger){
 
         TextView txtFullName = getView().findViewById(R.id.txtdriverFullName);
-        txtFullName.setText(String.format("%s %s", passenger.getName(), passenger.getLastName()));
+        txtFullName.setText(String.format("%s %s", passenger.getName(), passenger.getSurname()));
 
         TextView txtPhoneNumber = getView().findViewById(R.id.txtdriverPhoneNumber);
-        txtPhoneNumber.setText(passenger.getPhoneNumber());
+        txtPhoneNumber.setText(passenger.getTelephoneNumber());
 
         TextView txtEmailAddress = getView().findViewById(R.id.txtdriverEmail);
-        txtEmailAddress.setText(passenger.getEmailAddress());
+        txtEmailAddress.setText(passenger.getEmail());
 
         TextView txtAddress = getView().findViewById(R.id.txtDriverHomeAddress);
         txtAddress.setText(passenger.getAddress());
 
         TextView tvBlocked = getView().findViewById(R.id.txtDriverBlocked);
-        if(!passenger.isBlocked())
-            tvBlocked.setText("");
-        else {
-            tvBlocked.setText(R.string.blocked_text);
-            tvBlocked.setTextColor(Color.RED);
-        }
+        tvBlocked.setText("");
+        ImageView imgPassengerInfoAvatar = getActivity().findViewById(R.id.imgPassengerInfoAvatar);
+        if(!passenger.getProfilePicture().contains(",")){return;}
+        String base64Image = passenger.getProfilePicture().split(",")[1];
+        byte[] decodedString = Base64.decode(base64Image, Base64.DEFAULT);
+        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        imgPassengerInfoAvatar.setImageBitmap(decodedByte);
     }
 
     @Override
