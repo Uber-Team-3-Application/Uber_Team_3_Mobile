95888b4adb11c016b5efd0a89cafb87fc77dc36f
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2023-01-16 00:39:07 +0100

    updated rejection dialog

--- UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/dialogs/RejectionDialog.java
+++ UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/dialogs/RejectionDialog.java
@@ -1,21 +1,22 @@
 package com.example.uberapp_tim3.dialogs;
 
-import android.app.Dialog;
 import android.content.Context;
 import android.text.InputType;
 import android.widget.EditText;
 import androidx.appcompat.app.AlertDialog;
 
+import com.example.uberapp_tim3.activities.NewRideNotificationActivity;
+
 
 public class RejectionDialog extends AlertDialog.Builder {
 
 
-    public RejectionDialog(Context context, Dialog mainDialog) {
+    public RejectionDialog(Context context, NewRideNotificationActivity newRideNotificationActivity) {
         super(context);
-        setUpDialog(mainDialog);
+        setUpDialog(newRideNotificationActivity);
     }
 
-    private void setUpDialog(Dialog mainDialog) {
+    private void setUpDialog(NewRideNotificationActivity newRideNotificationActivity) {
         setTitle("Input reason of rejection");
         final EditText input = new EditText(getContext());
         input.setInputType(InputType.TYPE_CLASS_TEXT);
@@ -23,7 +24,8 @@ public class RejectionDialog extends AlertDialog.Builder {
 
         setPositiveButton("OK", (dialog, id) -> {
             dialog.dismiss();
-            mainDialog.cancel();
+            newRideNotificationActivity.finish();
+
         });
 
         setNegativeButton("CANCEL", (dialog, id) -> dialog.cancel());
