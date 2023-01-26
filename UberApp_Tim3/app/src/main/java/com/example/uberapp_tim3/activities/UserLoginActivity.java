439dbbb1deeb7b10f0b8579397a9f43b7bd30c40
Nebojša Vuga <72966399+nebojsavuga@users.noreply.github.com>
2023-01-26 22:40:03 +0100

    Update UserLoginActivity.java

--- UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/activities/UserLoginActivity.java
+++ UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/activities/UserLoginActivity.java
@@ -69,10 +69,10 @@ public class UserLoginActivity extends AppCompatActivity {
                 String etPw = ((EditText)findViewById(R.id.editTxtPassword)).getText().toString();
 
 
-  //              login("markopreradovic@gmail.com","Marko123");
-                login("marko@gmail.com","Marko123");
+                //login("markopreradovic@gmail.com","Marko123");
+                //login("marko@gmail.com","Marko123");
 
-                //login(etUser, etPw);
+                login(etUser, etPw);
 
 
             }
