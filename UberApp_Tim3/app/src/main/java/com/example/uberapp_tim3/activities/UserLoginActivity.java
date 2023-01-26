ac7935cd30f7b2d232ab9ea51500200a51760532
Jelena Mikovic <jelena.mikovic189@gmail.com>
2023-01-26 22:31:20 +0100

    Added inbox for driver

--- UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/activities/UserLoginActivity.java
+++ UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/activities/UserLoginActivity.java
@@ -69,8 +69,8 @@ public class UserLoginActivity extends AppCompatActivity {
                 String etPw = ((EditText)findViewById(R.id.editTxtPassword)).getText().toString();
 
 
-  //              login("markopreradovic@gmail.com","Marko123");
-                login("marko@gmail.com","Marko123");
+                login("markopreradovic@gmail.com","Marko123");
+       //         login("marko@gmail.com","Marko123");
 
                 //login(etUser, etPw);
 
