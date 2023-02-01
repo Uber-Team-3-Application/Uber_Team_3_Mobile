47fcc57edaceadc32514d4d2198c1089acfca928
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2023-02-01 11:54:27 +0100

    updated passenger-ride-history

--- UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/adapters/PassengerRideHistoryAdapter.java
+++ UberApp_Tim3/app/src/main/java/com/example/uberapp_tim3/adapters/PassengerRideHistoryAdapter.java
@@ -10,18 +10,25 @@ import android.widget.TextView;
 
 
 import com.example.uberapp_tim3.R;
+import com.example.uberapp_tim3.model.DTO.LocationDTO;
+import com.example.uberapp_tim3.model.DTO.Paginated;
+import com.example.uberapp_tim3.model.DTO.PassengerRideDTO;
+import com.example.uberapp_tim3.model.DTO.RideDTO;
+import com.example.uberapp_tim3.model.DTO.RouteDTO;
 import com.example.uberapp_tim3.model.mockup.Drive;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class PassengerRideHistoryAdapter extends BaseAdapter {
 
-    private Activity myActivity;
-    private List<Drive> rideHistory;
+    private final Activity myActivity;
+    private final List<PassengerRideDTO> rideHistory;
 
-    public PassengerRideHistoryAdapter(Activity fragment, List<Drive> rideHistory){
+    public PassengerRideHistoryAdapter(Activity fragment, Paginated<PassengerRideDTO> rideHistory){
         this.myActivity = fragment;
-        this.rideHistory = rideHistory;
+        this.rideHistory = rideHistory.getResults();
     }
 
     @Override
@@ -39,11 +46,11 @@ public class PassengerRideHistoryAdapter extends BaseAdapter {
         return i;
     }
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint({"SetTextI18n", "InflateParams"})
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         View vi=view;
-        Drive drive = rideHistory.get(i);
+        PassengerRideDTO drive = rideHistory.get(i);
 
         if(view==null)
             vi = myActivity.getLayoutInflater().inflate(R.layout.passenger_ride_history_item, null);
@@ -52,9 +59,22 @@ public class PassengerRideHistoryAdapter extends BaseAdapter {
         TextView path = (TextView)vi.findViewById(R.id.tvPassengerRideHistoryFromTo);
         TextView price = vi.findViewById(R.id.tvPassengerRideHistoryRidePrice);
 
-        time.setText(drive.getStartDrive() + "-" + drive.getEndDrive());
-        path.setText(drive.getrelation());
-        price.setText(Double.toString(drive.getPrice()));
+        RouteDTO route1 = drive.getLocations().get(0);
+        RouteDTO route2 =  drive.getLocations().get(drive.getLocations().size() - 1);
+
+        @SuppressLint("SimpleDateFormat")
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+        String duration = "";
+        if(drive.getStartTime() != null && drive.getEndTime() != null) {
+            Date startTime = drive.getStartTime();
+            Date endTime = drive.getEndTime();
+            duration = sdf.format(startTime) + " - " + sdf.format(endTime);
+        }
+        time.setText(duration);
+
+        path.setText(route1.getDeparture().getAddress() + " - " + route2.getDestination().getAddress());
+        price.setText(Double.toString(drive.getTotalCost()));
 
         return vi;
     }
